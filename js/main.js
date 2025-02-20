b4d6fe3006ef2424a4707ffa907ebef57b513ed7
Dinh Xuan Hien <124948520+hiendepzainn@users.noreply.github.com>
2025-02-20 16:12:18 +0700

    sort +text search

--- js/main.js
+++ js/main.js
@@ -6,31 +6,41 @@ showProducts(fixedPATH);
 showAllCategory();
 
 var select = document.querySelector("select");
+//Sắp xếp sản phẩm theo Tiêu chí
 select.addEventListener("change", () => {
   let pathNOW;
   switch (select.value) {
     case "default":
       pathNOW = fixedPATH;
       showProducts(pathNOW);
-      console.log(pathNOW);
       break;
     case "price-up":
-      pathNOW = fixedPATH + `?_sort=price&_order=asc`;
+      pathNOW = fixedPATH + `?_sort=price`;
       showProducts(pathNOW);
-      console.log(pathNOW);
       break;
     case "price-down":
-      pathNOW = fixedPATH + `?_sort=price&_order=desc`;
+      pathNOW = fixedPATH + `?_sort=-price`;
       showProducts(pathNOW);
-      console.log(pathNOW);
       break;
     case "discount-down":
-      pathNOW = fixedPATH + `?_sort=discountPercentage&_order=desc`;
+      pathNOW = fixedPATH + `?_sort=-discountPercentage`;
       showProducts(pathNOW);
-      console.log(pathNOW);
       break;
-    default:
-      pathNOW = fixedPATH; // Xử lý trường hợp không có lựa chọn hợp lệ
-      break;
+  }
+});
+
+var searchButton = document.querySelector("button");
+var searchInput = document.querySelector(".products-block input");
+console.log(searchButton);
+console.log(searchInput);
+//Tìm kiếm sản phẩm
+searchButton.addEventListener("click", () => {
+  if (searchInput.value == "") {
+    showProducts(fixedPATH);
+  } else {
+    console.log(2);
+    let pathNOW = fixedPATH + `?q=${searchInput.value}`;
+    console.log(pathNOW);
+    showProducts(pathNOW);
   }
 });
